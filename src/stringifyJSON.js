4ce1adae991d865f2a82455a9472abdcf357f81e
Lindsay Pond <lepond@gmail.com>
2015-03-06 15:43:07 -0800

    Completed cases for numbers, strings, booleans, and arrays; working on Objects.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,45 @@
 
 // but you don't so you're going to write it from scratch:
 
+var bookEnd = function (string, start, end) {
+	var yo = start + string + end;
+	return yo;
+};
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var string = "", i, key;
+	if (obj === undefined) {
+		return undefined;
+	}
+	if (obj === null) {
+		return 'null';
+	} else if (obj instanceof Array) {
+	  	for ( i = 0; i < obj.length; i++) {
+	  		string += (stringifyJSON(obj[i]));
+	  		if ( i < obj.length - 1 ) {
+	  			string += ",";
+	  		}
+	 	}
+  	string = bookEnd(string, "[", "]");
+  	} else if (typeof obj === "string") {
+	  	for (i = 0; i < obj.length; i++) {
+	  		string+= obj[i];
+	  	}
+  		string = bookEnd(string, "\"", "\"");
+  	} else if (obj instanceof Object) {
+  		for (key in obj) {
+	  		string += stringifyJSON(key) + ":" + stringifyJSON(obj[key])+",";
+	  	}
+  		string = string.slice(0,string.length-1);
+  		string = bookEnd(string, "{", "}");
+	} else if (typeof obj === "boolean") {
+		if (obj) {
+			string += "true";
+		} else {
+			string += "false";
+		}
+	} else if (typeof obj === "number") {
+		string += obj;
+	}
+ return string;
 };
